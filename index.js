06385b421b668e5efe332b47020793a944e9233a
Ramesh05 <rameshkannan240@gmail.com>
2020-03-04 19:21:33 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
- let a=prompt("enter your name");
+//  let a=prompt("enter your name");
 
- let b=prompt("starts with ");
- if (b=Number){
-     alert("please enter the name!")
-  }
- else{
- prompt("please enter the name!")
+//  let b=prompt("starts with ");
+//  if (b=Number){
+//      alert("please enter the name!")
+//   }
+//  else{
+//  prompt("please enter the name!")
  
- }
+//  }
 
-let c=prompt("ends with");
-if (c=Number){
-  alert("please enter the name!")
+// let c=prompt("ends with");
+// if (c=Number){
+//   alert("please enter the name!")
 
-  }
-  else{
-    prompt("please enter the name!")
+//   }
+//   else{
+//     prompt("please enter the name!")
 
-}
-let a2=parseint('m')
-let b2=parseint('h')
-if ((typeof a2 !=Number)||(typeof b2 !=Number))
-  {
-   alert("please enter the name!")
+// }
+// let a2=parseint('m')
+// let b2=parseint('h')
+// if ((typeof a2 !=Number)||(typeof b2 !=Number))
+//   {
+//    alert("please enter the name!")
 
-  }
-  else
-  {
+//   }
+//   else
+//   {
 
-    alert(`${(b.m,h)}`)
+//     alert(`${(b.m,h)}`)
 
-  }
+//   }
 
