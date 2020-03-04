ce07e6e195d11482400209eb4deecd33a0dcab4c
Ramesh05 <rameshkannan240@gmail.com>
2020-03-04 17:28:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-1-task-prompt-90030
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-1-task-prompt-90030)
---
+++ index.js
@@ -0,0 +1,25 @@
+ //let a=prompt("enter your name");
+
+ //let b=prompt("starts with ");
+ if (b=Number){
+ prompt("please enter the name!")
+ }
+ else{
+    alert("please enter the name!")
+ }
+
+//let c=prompt("ends with");
+
+//let a2=parseint('m')
+//let b2=parseint('h')
+if ((typeof a2 !=Number)||(typeof b2 !=Number))
+  {
+   alert("please enter the name!")
+
+  }
+  else
+  {
+
+    alert(`${(b.m,h)}`)
+
+  }
