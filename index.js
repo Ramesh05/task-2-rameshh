c12f804a7027358099950184d28914221de4f8a9
Ramesh05 <rameshkannan240@gmail.com>
2020-03-05 09:16:50 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 //  let a=prompt("enter your name");
 
 //  let b=prompt("starts with ");
-//  if (b=Number){
-//      alert("please enter the name!")
-//   }
-//  else{
-//  prompt("please enter the name!")
- 
-//  }
 
-// let c=prompt("ends with");
-// if (c=Number){
-//   alert("please enter the name!")
+//  let c=prompt("ends with");
+//  if (( b!="")||( c !=""))
+//    {
+    
+//        alert(`${a.slice(b,c)}`)
+//    }
+//    else
+//    {
 
-//   }
-//   else{
-//     prompt("please enter the name!")
+//     alert("please enter the name!") 
 
-// }
-// let a2=parseint('m')
-// let b2=parseint('h')
-// if ((typeof a2 !=Number)||(typeof b2 !=Number))
-//   {
-//    alert("please enter the name!")
-
-//   }
-//   else
-//   {
-
-//     alert(`${(b.m,h)}`)
-
-//   }
+//    }
 
